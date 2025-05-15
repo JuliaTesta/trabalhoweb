38b04e2f16152f24d946ddda25520ef946de0c3a
Julia Testa Matos <jutemts@gmail.com>
2025-05-15 14:58:11 -0300

    atualizado

--- myscript.js
+++ myscript.js
@@ -1,20 +1,25 @@
-let toggle = document.getElementById('toggle') //botao com id toggle
-let themeSystem = localStorage.getItem('themeSystem') || 'dark' //padrao
+let toggle = document.getElementById('toggle')                       //localiza no html o botao com id toggle
+let themeSystem = localStorage.getItem('themeSystem') || 'dark'      //verifica se ja existe um tema salvo no navegador.
 
-mudarTema(themeSystem); //chama a varial com o tema que esta no localStorage
+mudarTema(themeSystem);                                               //chama a função que aplica o tema atual 
 
 if(toggle){ //verifica se o botao existe
-    toggle.addEventListener('click', () => { //add determinada acao quando eu 'clicar' 
+    toggle.addEventListener('click', () => {  //add determinada acao quando eu 'clicar'. quando clicar vai acontecer oq   tem dentro dessa chave.
     let temaAtual =document.documentElement.getAttribute('data-theme'); //le o valor atual que esta em data-theme
 
-    let novoTema = temaAtual === 'dark' ? 'light' : 'dark';
+    if(temaAtual === 'dark'){
+        novoTema ='light'
+    }
+    else{
+        novoTema='dark'
+    }
     
    localStorage.setItem('themeSystem',novoTema); //salva o novo tema no localstorage 
    
    mudarTema(novoTema)
     })
-}
-function mudarTema(theme){
-    document.documentElement.setAttribute('data-theme',theme);
+}   
+   function mudarTema(theme){                                     //o parametro theme pode ser light ou dark
+    document.documentElement.setAttribute('data-theme',theme);      //se o theme for light, substitui as cores do dark 
 }
 
